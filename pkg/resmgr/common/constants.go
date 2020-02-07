a6305af75fabdb788e77cb3c18fadfba305806ac
linsicai <linsicai@gmail.com>
2020-02-07 11:10:19 +0800

    学习

--- pkg/resmgr/common/constants.go
+++ pkg/resmgr/common/constants.go
@@ -17,6 +17,8 @@ package common
 const (
 	// RunningStateNotStarted represents not started state of a resource manager component
 	RunningStateNotStarted = 0
+
+
 	// RunningStateRunning represents running state of a resource manager component
 	RunningStateRunning = 1
 )
